a686863f50edd88001513327f7d280db28366936
thorwebdev <thor@supabase.io>
2022-12-07 12:04:23 +0800

    chore: add no-check

--- supabase/functions/stripe-webhook/index.ts
+++ supabase/functions/stripe-webhook/index.ts
@@ -5,7 +5,7 @@
 import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
 
 // esm.sh is used to compile stripe-node to be compatible with ES modules.
-import Stripe from "https://esm.sh/stripe@11.2.0?target=deno";
+import Stripe from "https://esm.sh/stripe@11.2.0?target=deno&no-check";
 
 const stripe = Stripe(Deno.env.get("STRIPE_API_KEY"), {
   // This is needed to use the Fetch API rather than relying on the Node http
